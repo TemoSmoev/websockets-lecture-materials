217516a7527a2e27366a0b45434edae97adc601a
Temo Smoev <smoev1998@gmail.com>
2021-09-27 16:17:58 +0400

    built markup for WebSockets lecture

---
+++ client/scripts/main.js
@@ -0,0 +1,8 @@
+const messages = document.querySelector(".messenger__messages");
+const input = document.querySelector(".messenger__input");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    //code
+  }
+});
