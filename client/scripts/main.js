1b84bc803d0897d8920c7fda4405ff4e5dd65ebf
Temo Smoev <smoev1998@gmail.com>
2021-09-30 16:17:21 +0400

    added kinda server documentation

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,8 +1,25 @@
+const url = "ws://localhost:8080";
 const messages = document.querySelector(".messenger__messages");
 const input = document.querySelector(".messenger__input");
+const user = document.querySelector(".messenger__user");
+const onlineDot = document.querySelector(".messenger__dot");
 
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    //code
+    //send message
   }
 });
+
+/* 
+        
+Documentation
+url:"ws://localhost:8080"
+
+  get {type:'GET_ID',payload: {id:(string)}}
+
+  get {type:'NEW_MESSAGE', payload:{message:(string),userId:(string)}}
+
+  send {type:'NEW_MESSAGE', payload:{message:(string)}}
+
+
+*/
