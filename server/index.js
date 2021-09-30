a5374d7c4dd773648a8b55dcd9e2e4ddb895eba3
Temo Smoev <smoev1998@gmail.com>
2021-09-30 13:26:12 +0400

    refactored markup, added server side

---
+++ server/index.js
@@ -0,0 +1,23 @@
+//SERVER
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 }, () => {
+  console.log("our server is running");
+});
+
+wss.on("connection", (ws) => {
+  const id = "User-" + Date.now().toString().slice(-6);
+  ws.id = id;
+  ws.send(JSON.stringify({ type: "SET_ID", payload: id }));
+
+  ws.onmessage = ({ data }) => {
+    const { type, payload } = JSON.parse(data);
+
+    if (type === "NEW_MESSAGE") {
+      wss.clients.forEach((client) => {
+        if (client.id !== ws.id)
+          client.send(JSON.stringify({ type: "NEW_MESSAGE", payload: { message: payload, userId: ws.id } }));
+      });
+    }
+  };
+});
